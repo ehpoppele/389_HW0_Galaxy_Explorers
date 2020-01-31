496e1383396d1173acec3483f4b46c18c1749322
Eli Poppele <ehpoppele@gmail.com>
2020-01-31 21:02:05 +0000

    finished int type data

--- update_script.cc
+++ update_script.cc
@@ -6,12 +6,14 @@
 #include <fstream>
 #include <string>
 
-std::vector<double> generate_random_list(int size, double bound, int seed)
+using data_t = double;
+
+std::vector<data_t> generate_random_list(int size, int bound, int seed)
 {
-    std::vector<double> ret = {};
+    std::vector<data_t> ret = {};
     //seed with size when func is called, then use uniform real distribution
     std::mt19937 gen(seed); //don't understand what this is entirely but it was on the c++ ref
-    std::uniform_real_distribution<> rng((0.0-bound), bound);
+    std::uniform_real_distribution<> rng(-bound, bound);
     for(int i = 0; i < size; i++){
         ret.push_back(rng(gen));
     }
@@ -19,7 +21,7 @@ std::vector<double> generate_random_list(int size, double bound, int seed)
 }
 
 //Update position by velocity, one time-step
-void update_coords(std::vector<double> xs, std::vector<double> ys, std::vector<double> zs, std::vector<double> vx, std::vector<double> vy, std::vector<double> vz)
+void update_coords(std::vector<data_t> xs, std::vector<data_t> ys, std::vector<data_t> zs, std::vector<data_t> vx, std::vector<data_t> vy, std::vector<data_t> vz)
 {
     for(int i = 0; i < xs.size(); i++){
         xs[i] = xs[i] + vx[i];
@@ -35,7 +37,7 @@ void update_coords(std::vector<double> xs, std::vector<double> ys, std::vector<d
 int main(){
 
 
-    std::vector<double> chksum_array = {}; //Holds all the checksums, so that I can check them automatically on later loops, and don't have to include them in output. Chksum for size is at [size_p-8].
+    std::vector<data_t> chksum_array = {}; //Holds all the checksums, so that I can check them automatically on later loops, and don't have to include them in output. Chksum for size is at [size_p-8].
     std::vector<double> time_array = {}; //Holds all the times in a similar manner, updating them when a new minimum is found
 
     for(int i = 0; i < 5; i++){//Do five times; record minimums and confirm checksums
@@ -44,12 +46,12 @@ int main(){
         while (size_p < 25){
             int size = pow(2, size_p);
             int iters = pow(2, iters_p);
-            std::vector<double> xs = generate_random_list(size, 1000.0, size);
-            std::vector<double> ys = generate_random_list(size, 1000.0, size);
-            std::vector<double> zs = generate_random_list(size, 1000.0, size);
-            std::vector<double> vx = generate_random_list(size, 1.0, size);
-            std::vector<double> vy = generate_random_list(size, 1.0, size);
-            std::vector<double> vz = generate_random_list(size, 1.0, size);
+            std::vector<data_t> xs = generate_random_list(size, 1000.0, size);
+            std::vector<data_t> ys = generate_random_list(size, 1000.0, size);
+            std::vector<data_t> zs = generate_random_list(size, 1000.0, size);
+            std::vector<data_t> vx = generate_random_list(size, 1.0, size);
+            std::vector<data_t> vy = generate_random_list(size, 1.0, size);
+            std::vector<data_t> vz = generate_random_list(size, 1.0, size);
             
             std::clock_t start;
             double elapsed;
@@ -61,7 +63,7 @@ int main(){
             elapsed = (std::clock() - start ) / (double) CLOCKS_PER_SEC; //Stackoverflow informs me I should be able to just call this var
             
             //use accumulate for checksum
-            double chksum = std::accumulate(xs.begin(), xs.end(), 0.0) + std::accumulate(ys.begin(), ys.end(), 0.0) + std::accumulate(zs.begin(), zs.end(), 0.0);
+            data_t chksum = std::accumulate(xs.begin(), xs.end(), 0.0) + std::accumulate(ys.begin(), ys.end(), 0.0) + std::accumulate(zs.begin(), zs.end(), 0.0);
             
             if(i == 0){ //First time through, record chksum and time
                 chksum_array.push_back(chksum);
@@ -82,7 +84,7 @@ int main(){
     }
 
     //write to file
-    std::ofstream outfile ("cpp_double_times.txt");
+    std::ofstream outfile ("cpp_t_times.txt"); //Change this each time too
     if(outfile.is_open()){
         //iterate through the time array, grabbing out numbers by size
         for(int size_p = 8; size_p <25; size_p++){
