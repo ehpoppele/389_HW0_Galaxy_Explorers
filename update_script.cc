527a998cdeb17b034580b342c9b7f190a1ee2b8d
Eli Poppele <ehpoppele@gmail.com>
2020-02-09 23:15:05 +0000

    Added testing in main file

--- update_script.cc
+++ update_script.cc
@@ -6,8 +6,13 @@
 #include <fstream>
 #include <string>
 
-using data_t = double;
+//This file works essentially as a c++ replica of the python script file, with
+//essentially the same functions and tasks, but it also includes a testing functions
+//which can be ran by using args with the program command
 
+using data_t = double; //So I can easily test different int/double types in the program
+
+//fill a vector with numbers within |bound| of zero
 std::vector<data_t> generate_random_list(int size, int bound, int seed)
 {
     std::vector<data_t> ret = {};
@@ -31,16 +36,66 @@ void update_coords(std::vector<data_t>& xs, std::vector<data_t>& ys, std::vector
     return;
 }
 
+//test function, can be called using 't' when running the program
+//testing decisions are explained in the readme
+bool test_coords()
+{
+    bool success = true;
+    //first check the generate_random_list func
+    std::vector<data_t> rng_list = generate_random_list(100.0, 1000.0, 100.0);
+    if( rng_list.size() != 100){
+        std::cout << "Length of generated random list is wrong." << std::endl;
+        success = false;
+    }
+    if(rng_list[1] == rng_list[99]){ //if randomized, there is a very low chance these two will be the same
+        std::cout << "Random list does not appear to be random." << std::endl;
+        success = false;
+    }
+    for(int i = 0; i < 100; i++){
+        if(rng_list[i] > 1000.0 or rng_list[i] < -1000.0){
+            std::cout << "Random values appear outside of given bounds." << std::endl;
+            success = false;
+        }
+    }
+    //Now check the update_coords func
+    std::vector<data_t> xs(3,100.0);
+    std::vector<data_t> ys(3,100.0);
+    std::vector<data_t> zs(3,100.0);
+    std::vector<data_t> vx(3,5.0);
+    std::vector<data_t> vy(3,10.0);
+    std::vector<data_t> vz(3,-5.0);
+    update_coords(xs, ys, zs, vx, vy, vz);
+    if(xs[0] != 105.0 or ys[1] != 110.0 or zs[1] != 95.0){
+        std::cout << "Updating with velocities didn't work as expected." << std::endl;
+        success = false;
+    }
+    return success;
+}
+
+
 //-----------------------------
 
 //Loop through all the sizes and scale down the iters as appropriate, now as the main func
-int main(){
-
+int main(int argc, const char * argv[]){
+    //check if using test commands, otherwise run the normal script
+    if(argc > 1){
+        if(*argv[1] == 't'){ //seems that and isn't shortcircuited, so I have to put this as a separate condition
+        if(test_coords() == false){
+            std::cout << "Test has failed." << std::endl;
+        } else {
+            std::cout << "Test succeeded!" << std::endl;
+        }
+    } else {
+        std::cout << "It seems that argument isn't recognized." << std::endl;
+    }
+    } else {
 
     std::vector<data_t> chksum_array = {}; //Holds all the checksums, so that I can check them automatically on later loops, and don't have to include them in output. Chksum for size is at [size_p-8].
     std::vector<double> time_array = {}; //Holds all the times in a similar manner, updating them when a new minimum is found
 
     for(int i = 0; i < 5; i++){//Do five times; record minimums and confirm checksums
+        //we start at the smallest size used of 2^8, and have 2^17 iterations for this;
+        //as described in the readme, these then scale in opposite directions as the loop continues
         int size_p = 8;
         int iters_p = 17;
         while (size_p < 25){
@@ -53,13 +108,13 @@ int main(){
             std::vector<data_t> vy = generate_random_list(size, 1.0, size);
             std::vector<data_t> vz = generate_random_list(size, 1.0, size);
             
+            //timing using std clock functions
             std::clock_t start;
             double elapsed;
             start = std::clock();
             for(int i = 0; i < iters; i++){
                 update_coords(xs, ys, zs, vx, vy, vz);
             }
-            //fin = std::clock();
             elapsed = (std::clock() - start ) / (double) CLOCKS_PER_SEC; //Stackoverflow informs me I should be able to just call this var
             
             //use accumulate for checksum
@@ -72,11 +127,12 @@ int main(){
                 if(chksum_array[size_p - 8]!=chksum){
                     std::cout << "Checksums don't match!" << std::endl;
                 }
+                //push to the array in microseconds, so we can print to file later
                 if (time_array[size_p - 8] > (1000000.0 * elapsed / (size * iters))){
                     time_array[size_p - 8] = (1000000.0 * elapsed / (size * iters));
                 }
             }
-            std::cout<<size_p<<"    "<<elapsed<<"\n";
+            std::cout<<size_p<<"    "<<elapsed<<std::endl;
             size_p += 1;
             iters_p -= 1;
             
@@ -94,6 +150,7 @@ int main(){
             outfile << std::to_string(time_array[size_p-8]) << std::endl;
         }
         outfile.close();
+    }
     }
     return 0;
 }
